c22c1d3d0f5f115ecc778134ef94e96805a1878e
Gordon Yu <gyunit@gmail.com>
2020-03-20 00:23:50 -0700

    add event listener onClick for letters that, when clicked, deletes letter from input

--- src/App.js
+++ src/App.js
@@ -13,12 +13,21 @@ class App extends Component {
     this.setState({userInput: event.target.value});
   }
 
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  }
+
   render() {
 
     const charList = this.state.userInput.split('').map((char, index) => {
-      return <Char character={char} key={index} />;
+      return <Char 
+        character={char} 
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />;
     });
-
 
     return (
       <div className="App">
